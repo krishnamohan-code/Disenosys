ab863f60c736710b7062097e0002faf4901c35b7
krishnamohan <krishnamohan.py@gmail.com>
2020-09-17 11:11:02 +0530

    Added

--- index.js
+++ index.js
@@ -78,4 +78,20 @@ function myFunction() {
       // TO MAKE IT ALL NEAT 
   
   });
+
+
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+
+  
   
