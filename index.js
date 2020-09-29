ec91fe2a56fb3b9fbaaef01720d4bff6c935a807
krishnamohan <krishnamohan.py@gmail.com>
2020-09-29 11:11:18 +0530

    nav-bar updated

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // nav start 
-$('.menubtn').on('click', function() {
-  $('nav > ul').slideToggle().trigger('click');
+$(document).ready(function(){ 
+	var touch 	= $('#resp-menu');
+	var menu 	= $('.menu');
+ 
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+	
 });
-
 // nav end
   jQuery(document).ready(function($) {
     "use strict";
